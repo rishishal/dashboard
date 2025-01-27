9dbbc88372a69c461460577093c2f4418dc00148
rishiVaraha <rishi.kumar@varahaworld.com>
2025-01-27 18:36:57 +0530

    adjust height

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -1,4 +1,4 @@
-import { Check, CircleCheck } from "lucide-react";
+import { CircleCheck } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 
 export function ProgressBar() {
@@ -17,7 +17,19 @@ export function ProgressBar() {
   return (
     <div className="relative flex items-center justify-between">
       <Separator orientation="vertical" className="h-80 w-2 rounded-md" />
-      <div className="absolute flex flex-col top-0 -left-2 gap-20">
+      <Separator
+        orientation="vertical"
+        className="absolute top-0 w-2 rounded-md bg-green-500"
+        style={{ height: `calc(${progressWidth}% - 20px)` }}
+      />
+
+      {/* <div className="relative w-full h-2 bg-gray-200 rounded-md">
+        <div
+          className="absolute h-full bg-green-500 rounded-md"
+          style={{ width: `${progressWidth}%` }}
+        />
+      </div> */}
+      <div className="absolute flex flex-col top-0 -left-2 gap-20 rounded-full">
         {steps.map((step, index) => {
           const isCompleted = index < currentStep;
           return (
