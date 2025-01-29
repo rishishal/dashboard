bbe8034beffca34cdc8cd59c7a667bfccadcc96c
rishiVaraha <rishi.kumar@varahaworld.com>
2025-01-29 16:18:52 +0530

    add chart

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,12 +3,14 @@ import { InfoCards } from "./components/info-card";
 import { Ordercolumns } from "./components/Recent-Table/column";
 import { RecentData } from "./recentData";
 import { RecentDataTable } from "./components/Recent-Table/DataTable";
+import { SalesChart } from "@/components/charts/area-chart";
 const DasboardHome = () => {
   return (
     <div className="flex flex-col gap-10 p-4">
       <div className="flex justify-start gap-6">
         <InfoCards />
       </div>
+      <SalesChart />
 
       <Card className="space-y-4 p-4">
         <h2 className="font-bold text-2xl text-primary/85">Recent Order</h2>
